0ae596aec7f1a634ffe9cbe2dc875d7879c344f0
Imran shaikh <imranpathan7040@gmail.com>
2024-01-28 15:11:21 +0530

    routes added

---
+++ src/components/AboutPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function AboutPage() {
+  return (
+    <div>
+        <h1>About</h1>
+        <p>This is a simple website created using the following technologies:</p>
+    </div>
+  )
+}
+
+export default AboutPage
--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,32 @@
 
 import React from "react";
 import './../styles/App.css';
+import Homepage from "./Homepage";
+import AboutPage from "./AboutPage";
+import  { BrowserRouter, Routes,Route ,NavLink} from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        
+        <BrowserRouter>
+        <ul>
+          <NavLink to="/">
+          <li>Home</li>
+          </NavLink>
+            
+          <NavLink to="/about">
+          <li>About us</li>
+
+          </NavLink>
+        </ul>
+          <Routes>
+
+            <Route path="/" element={<Homepage/>}/>
+            <Route path="/about" element={<AboutPage/>}/>
+          </Routes>
+        </BrowserRouter>
     </div>
   )
 }
